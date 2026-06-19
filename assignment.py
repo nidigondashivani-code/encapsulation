490068de80f697beab50ef78cfa93c9c9e00c0d3
Shivani Nidigonda <nidigondashivani@gmail.com>
2026-06-19 12:40:18 +0530

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -1,58 +1,45 @@
 class GameCharacter:
-    def __init__(self,health=100,energy=50):
-        self.__health=health
-        self.__energy=energy
-    
-    def attack(self):
-        if self.__energy>=10:
-            self.__energy-=10
-            print("Attacking with power")
-        else:
-            print("Not enough energy to attack.")
 
-    def take_damage(self,amount):
+    def __init__(self):
+        self.__health = 100
+        self.__energy = 50
+
+    def attack(self):
+        if self.__energy >= 10:
+            self.__energy -= 10
+            print("Attack performed!")
+        else:
+            print("Not enough energy!")
+
+    def take_damage(self, amount):
         self.__health -= amount
-        if self.__health<0:
-            self.__health=0
+
+        if self.__health < 0:
+            self.__health = 0
+
+    def heal(self, amount):
+        self.__health += amount
+
+        if self.__health > 100:
+            self.__health = 100
+
+    def rest(self):
+        self.__energy += 20
+
+        if self.__energy > 100:
+            self.__energy = 100
+
     def get_status(self):
-        return f"Health:{self.__health},{self.__energy}"
-    
-    def get_energy(self):
-        return self.__energy
-    
-    def get_health(self):
-        return self.__health
-    
-    def set_energy(self,energy):
-        self.__energy=energy
+        return f"Health: {self.__health}, Energy: {self.__energy}"
 
-    def set_health(self,health):
-        self.__health=health
 
-class Warrior(GameCharacter):
+# Test Code
+hero = GameCharacter()
 
-    def attack(self):
-        super().attack()
-        print("Warrior's attack is powerful!")
-class Mage(GameCharacter):
-    def attack(self):
-        if self.get_energy()>=20:
-            self.set_energy(self.get_energy()-20)
-            print("Mage's attack is magical!")
-        else:
-            print("Not enough energy to attack.")
-    def heal(self,amount):
-        new_health=self.get_health()+amount
+hero.attack()
+hero.attack()
+hero.take_damage(30)
+hero.heal(20)
+hero.rest()
 
-        if new_health>100:
-            new_health=100
-        self.set_health(new_health)
-
-w=Warrior()
-m=Mage()
-w.attack()
-m.attack()
-w.take_damage(30)
-m.heal(20)
-print(w.get_status())
-print(m.get_status())
+print(hero.get_status())
